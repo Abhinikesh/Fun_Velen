9d9107d41a7c5f014cf61509aee12ab84cfc197f
Abhinikesh <abhinikeshkumar1@gmail.com>
2026-02-14 14:40:19 +0530

    Correct banner image filename case sensitivity

--- public/js/script.js
+++ public/js/script.js
@@ -33,7 +33,7 @@ no_button.addEventListener('click', () => {
     // Change banner source
     let banner = document.getElementById('banner');
     if (clicks === 0) {
-        banner.src = "public/images/no.gif";
+        banner.src = "public/images/No.gif";
         refreshBanner();
     }
     clicks++;
